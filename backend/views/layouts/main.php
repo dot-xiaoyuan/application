c621e938653e9683ea84ba967264f08e44856bac
小原 <yt.vertigo0927@gmail.com>
2024-06-29 23:22:24 +0800

    sidebar

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -26,25 +26,34 @@ TablerAsset::register($this);
     <body class="d-flex flex-column h-100">
     <?php $this->beginBody() ?>
 
-    <header>
+    <div class="page">
+        <!-- Sidebar -->
         <?php
         NavBar::begin([
             'brandLabel' => Yii::$app->name,
             'brandUrl' => Yii::$app->homeUrl,
             'options' => [
-                'class' => 'navbar navbar-expand-md navbar-dark bg-dark fixed-top',
+                'class' => 'navbar navbar-vertical navbar-expand-lg',
+                'data-bs-theme' => 'dark',
             ],
         ]);
         $menuItems = [
-            ['label' => 'Home', 'url' => ['/site/index']],
+            ['label' => '<span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+                         stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                         stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path
+                            d="M5 12l-2 0l9 -9l9 9l-2 0"/><path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"/><path
+                            d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6"/></svg>
+                  </span>'.'Home', 'url' => ['/site/index'], 'encode' => false,],
         ];
         if (Yii::$app->user->isGuest) {
             $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
         }
         echo Nav::widget([
-            'options' => ['class' => 'navbar-nav me-auto mb-2 mb-md-0'],
+            'options' => ['class' => 'navbar-nav pt-lg-3'],
             'items' => $menuItems,
         ]);
+
         if (Yii::$app->user->isGuest) {
             echo Html::tag('div', Html::a('Login', ['/site/login'], ['class' => ['btn btn-link login text-decoration-none']]), ['class' => ['d-flex']]);
         } else {
@@ -57,8 +66,7 @@ TablerAsset::register($this);
         }
         NavBar::end();
         ?>
-    </header>
-
+    </div>
     <main role="main" class="flex-shrink-0">
         <div class="container">
             <?= Breadcrumbs::widget([
