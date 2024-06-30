61a77693aea0b510aa673bc27bbf0e28b4d36730
小原 <yt.vertigo0927@gmail.com>
2024-06-30 21:21:55 +0800

    添加libs asset

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -81,7 +81,14 @@ use yii\web\View;
                         <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown"
                            aria-label="Open user menu">
                             <span class="avatar">
-                                <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-user"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                     fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                     stroke-linejoin="round"
+                                     class="icon icon-tabler icons-tabler-outline icon-tabler-user"><path stroke="none"
+                                                                                                          d="M0 0h24v24H0z"
+                                                                                                          fill="none"/><path
+                                            d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"/><path
+                                            d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"/></svg>
                             </span>
                             <div class="d-none d-xl-block ps-2">
                                 <div><?= Yii::$app->user->identity->username ?></div>
@@ -141,7 +148,9 @@ use yii\web\View;
                 </div>
             </div>
             <div class="page-body">
-                <?= $content ?>
+                <div class="container-xl">
+                    <?= $content ?>
+                </div>
             </div>
             <footer class="footer footer-transparent d-print-none">
                 <div class="container-xl">
